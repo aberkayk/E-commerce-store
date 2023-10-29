b8d4f07bf52082a89882997492d11b157b93529e
aberkayk <aberkayk@gmail.com>
2023-10-29 09:57:23 +0300

    Add billboard and products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard(
+    "23a6a834-4f4b-4066-9af7-cf8820150ce2"
+  );
+  const products = await getProducts({ isFeatured: true });
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
